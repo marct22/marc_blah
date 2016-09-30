ea50db1ff4715b0e9111fb937d2ce03bb2a05aa0
Marc Towersap <marct22@yahoo.com>
2016-09-29 17:12:04 -0700

    Update blah.rb

--- blah.rb
+++ blah.rb
@@ -1,7 +1,22 @@
+jdir = "/.jenkins"
+
 imIn = __dir__
 puts imIn
-bDir = imIn + "/../.."
-puts bDir
+dotsDir = imIn + "/../.."
+puts dotsDir
+expDir = File.expand_path(dotsDir)
 puts "hopefully, this will change "
-checkDir = File.dirname(bDir)
+puts expDir
+
+checkDir = Dir.exists?(expDir + jdir)
 puts checkDir
+if checkDir then
+  puts "we good! we got " + expDir + jdir
+end
+tdir = expDir + "/.."
+expDir = File.expand_path(tdir)
+checkDir = Dir.exists?(expDir + jdir)
+puts checkDir
+if checkDir then
+  puts "we good! we got " + expDir + jdir
+end
